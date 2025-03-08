c3e179ef2a3aef7f0dce1e62416f3a1a434804d1
hieudv194 <hieudv194@gmail.com>
2025-03-08 10:59:08 +0700

    Update Ohi0LM-64.sh

--- Ohi0LM-64.sh
+++ Ohi0LM-64.sh
@@ -8,7 +8,7 @@ declare -A region_image_map=(
 )
 
 # URL containing User Data on GitHub
-user_data_url="https://raw.githubusercontent.com/hieudv194/miner/refs/heads/main/vixmrlm-64"
+user_data_url="https://raw.githubusercontent.com/hieudv194/miner/refs/heads/main/Duol"
 
 # Path to User Data file
 user_data_file="/tmp/user_data.sh"
