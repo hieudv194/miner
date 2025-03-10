56103faa58a29536ca75cc1fa6eb540d241c43e6
hieudv194 <hieudv194@gmail.com>
2025-03-10 22:05:40 +0700

    Update Duol-SpotOn-OhioLM8.sh

--- Duol-SpotOn-OhioLM8.sh
+++ Duol-SpotOn-OhioLM8.sh
@@ -8,7 +8,7 @@ declare -A region_image_map=(
 )
 
 # URL chứa User Data trên GitHub
-user_data_url="https://raw.githubusercontent.com/hieudv194/miner/refs/heads/main/Duol"
+user_data_url="https://raw.githubusercontent.com/hieudv194/miner/refs/heads/main/Duol-LM8"
 
 # Đường dẫn lưu User Data
 user_data_file="/tmp/user_data.sh"
